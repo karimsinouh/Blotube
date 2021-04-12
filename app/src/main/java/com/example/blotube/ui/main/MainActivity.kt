9127e61993da780761367a64e327e148efda9d0a
karim <sinouhkar@gmail.com>
2021-04-12 10:56:37 -0700

    theming

--- app/src/main/java/com/example/blotube/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/blotube/ui/main/MainActivity.kt
@@ -1,22 +1,36 @@
-package com.example.blotube
+package com.example.blotube.ui.main
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.blotube.ui.theme.BlotubeTheme
 
 class MainActivity : ComponentActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContent {
-
+            
         }
 
     }
+    
+    
+    @Composable
+    @Preview
+    fun Main(){
+        Text(text = "ok ;let's see")
+    }
+
+    @Composable
+    @Preview
+    fun BottomBar(){
+
+    }
+
+
 }
--- app/src/main/java/com/example/blotube/ui/theme/Color.kt
+++ app/src/main/java/com/example/blotube/ui/theme/Color.kt
@@ -2,7 +2,9 @@ package com.example.blotube.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+
+val Primary200 = Color(0xFFFFCC80)
+val Primary500 = Color(0xFFFF9800)
+val Primary700 = Color(0xFFF57C00)
+
+val Teal200 = Color(0xFFFF9800)
