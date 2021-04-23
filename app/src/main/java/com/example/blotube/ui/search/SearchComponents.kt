abdf044ca1cfe11828ef58952d0c2f13753d2c27
karim <sinouhkar@gmail.com>
2021-04-23 06:55:53 -0700

    clear read & watch later

--- app/src/main/java/com/example/blotube/ui/search/SearchComponents.kt
+++ app/src/main/java/com/example/blotube/ui/search/SearchComponents.kt
@@ -1,25 +1,24 @@
 package com.example.blotube.ui.search
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.outlined.*
+import androidx.compose.material.icons.outlined.Book
+import androidx.compose.material.icons.outlined.FilterList
+import androidx.compose.material.icons.outlined.Info
+import androidx.compose.material.icons.outlined.Search
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.blotube.R
 import com.example.blotube.data.blogger.Blog
 import com.example.blotube.data.youtube.items.SearchItem
-import com.example.blotube.data.youtube.items.VideoItem
 import com.example.blotube.ui.theme.ImagePlaceholder
 import com.example.blotube.ui.theme.RoundedShape
 import com.google.accompanist.coil.CoilImage
@@ -147,7 +146,7 @@ fun SearchFilterDialogContent(
         selectedColor = MaterialTheme.colors.primary
     )
 
-    Column(Modifier.width(250.dp)) {
+    Column(Modifier.width(150.dp)) {
         Row(
             verticalAlignment = Alignment.CenterVertically,
             modifier = Modifier.padding(8.dp)
--- app/src/main/java/com/example/blotube/ui/settings/Settings.kt
+++ app/src/main/java/com/example/blotube/ui/settings/Settings.kt
@@ -1,18 +1,20 @@
 package com.example.blotube.ui.settings
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.*
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.blotube.R
 import com.example.blotube.api.database.Database
 import com.example.blotube.util.NightMode
 import kotlinx.coroutines.launch
@@ -23,27 +25,24 @@ fun Settings(
     db: Database
     ){
 
+    val readLaterState=remember{ mutableStateOf(false) }
+    val watchLaterState=remember{ mutableStateOf(false) }
+
     val c= LocalContext.current
 
     val scope= rememberCoroutineScope()
-
-    val scrollState= rememberScrollState()
 
     val switchColor=SwitchDefaults.colors(checkedThumbColor = MaterialTheme.colors.primary)
 
     val nightMode=NightMode.isEnabled(c).collectAsState(initial = false)
 
-    Column(
-        Modifier
-            .fillMaxSize()
-            .verticalScroll(scrollState)
-    ) {
+    Column(Modifier.fillMaxSize()) {
 
         ListItem (
             icon= { Icon(imageVector = Icons.Outlined.DarkMode, contentDescription = "") }
         ){
             Row {
-                Text(text = "Dark Theme",
+                Text(stringResource(R.string.night_mode),
                     Modifier
                         .fillMaxWidth()
                         .weight(0.9f))
@@ -60,26 +59,82 @@ fun Settings(
 
         Divider()
         ListItem(
-            icon= { Icon(imageVector = Icons.Outlined.Book, contentDescription = "") }
-        ) {
-            Text(text = "Clear Read Later")
+            icon= { Icon(imageVector = Icons.Outlined.Book, contentDescription = "") },
+            modifier = Modifier.clickable {
+                readLaterState.value=true
+            }) {
+            Text(stringResource(R.string.clear_read_later))
         }
 
 
         Divider()
         ListItem(
-            icon= { Icon(imageVector = Icons.Outlined.WatchLater, contentDescription = "") }
-
+            icon= { Icon(imageVector = Icons.Outlined.WatchLater, contentDescription = "") },
+            modifier = Modifier.clickable {
+                watchLaterState.value=true
+            }
         ) {
-            Text(text = "Clear Watch Later")
+            Text(stringResource(R.string.clear_watch_later))
         }
 
         Divider()
         ListItem(
             icon= { Icon(imageVector = Icons.Outlined.Feedback, contentDescription = "") }
-        ) {
-            Text(text = "Feedback")
+            ) {
+            Text(text = stringResource(R.string.feedback))
         }
     }
+
+
+    //dialogs
+
+    if (watchLaterState.value)
+        ConfirmDialog(
+            stringResource(R.string.clear_watch_later),
+            text = stringResource(R.string.clear_watch_later_text) ,
+            onDismiss = { watchLaterState.value=false })
+        {
+            scope.launch {
+                db.videos().deleteAll()
+            }
+        }
+
+
+    if (readLaterState.value)
+        ConfirmDialog(
+            stringResource(R.string.clear_read_later),
+            text = stringResource(R.string.clear_read_later_text) ,
+            onDismiss = { readLaterState.value=false })
+        {
+            scope.launch {
+                db.posts().deleteAll()
+            }
+        }
     
+}
+
+@Composable
+fun ConfirmDialog(
+    title:String,
+    text:String,
+    onDismiss:()->Unit,
+    onConfirm:()->Unit
+){
+    AlertDialog(
+        onDismissRequest = onDismiss,
+        title = { Text(title) },
+        text= { Text(text) },
+        buttons = {
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp),
+                contentAlignment = Alignment.CenterEnd
+            ){
+                TextButton(onClick = onConfirm) {
+                    Text(stringResource(R.string.confirm))
+                }
+            }
+        }
+    )
 }
